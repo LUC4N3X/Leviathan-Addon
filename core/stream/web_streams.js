a7424ad4eae5544bcdbb9ae1208ab0bafb38da11
LUCA <luca.testing96@gmail.com>
2026-04-29 20:26:17 +0200

    Delete core directory

--- core/stream/web_streams.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const { createWebProviderTools } = require('./web_providers');
-
-function createWebStreamTools(deps) {
-    return createWebProviderTools(deps);
-}
-
-module.exports = { createWebStreamTools };
